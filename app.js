39fe9c70f51838aa1e4e453475fd3fdf6d47b5c2
sooheyon <sooheyonjo@gmail.com>
2023-11-23 15:51:28 +0900

    count 예제

--- app.js
+++ app.js
@@ -4,8 +4,15 @@ const app = express();
 
 const port = 3010;
 
+let count = 0;
+
 app.get("/", (req, res) => {
   return res.send("Hello, Express!");
+});
+
+app.get("/count", (req, res) => {
+  count++;
+  return res.json({ count: count });
 });
 
 app.listen(port, () => {
